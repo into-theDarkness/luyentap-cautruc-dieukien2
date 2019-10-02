50dc26dec9e20dbe14313f44fb5fbe889e62ad96
into-theDarkness <zzpeterpan0510@gmail.com>
2019-10-02 21:00:45 +0700

    xong bai tinh tien dien

---
+++ tiendien.js
@@ -0,0 +1,20 @@
+function tinhtiendien() {
+let sodien = document.getElementById("sodien").value;
+let result = 0;
+if (sodien <= 50){
+    result = sodien*1500
+    document.getElementById("hehe").innerHTML = + result +" VND";
+} else if (sodien > 50 && sodien <= 100){
+    result = (sodien -50)*3000 + 50*1500;
+    document.getElementById("hehe").innerHTML = + result + " VND";
+} else if (sodien >100 && sodien <= 200){
+    result = (sodien-100)*3500 + 50*1500 + 50*3000;
+    document.getElementById("hehe").innerHTML = + result + " VND";
+}else {
+    result = (sodien - 200)*5000 + 100*3500 + 50*3000 + 50*1500;
+    document.getElementById("hehe").innerHTML = + result + " VND";
+}
+
+
+    
+}
